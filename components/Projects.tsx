abf340e2a22f458df6c9351523336714486a590c
Airtas <89629005+ArisandiSatria@users.noreply.github.com>
2026-07-17 17:10:02 +0700

    update: added new project

--- components/Projects.tsx
+++ components/Projects.tsx
@@ -1,71 +1,8 @@
+import { PROJECTS } from "@/constants/PROJECTS";
 import { ExternalLink, Code2 } from "lucide-react";
 
 export default function Projects() {
-  const projects = [
-    {
-      title: "Workzenn",
-      description:
-        "AI-powered career preparation platform that empowers users to build professional resumes and practice their skills through an interactive mock interview simulator.",
-      technologies: [
-        "Next JS",
-        "Clerk",
-        "AI Integration",
-        "Tailwind CSS",
-        "Responsive Design",
-      ],
-      liveUrl: "http://workzenn.vercel.app/",
-      codeUrl: "https://github.com/arisandisatria/workzenn.git",
-      image: "/workzenn.png",
-    },
-    {
-      title: "CreatorPal",
-      description:
-        "AI-powered assistant platform specifically designed to help content creators, digital marketers, and business owners accelerate their content production process.",
-      technologies: [
-        "Next JS",
-        "Clerk",
-        "AI Integration",
-        "Tailwind CSS",
-        "Responsive Design",
-      ],
-      liveUrl: "https://creatorpal-ai.vercel.app/",
-      codeUrl: "https://github.com/arisandisatria/creatorpal.git",
-      image: "/creatorpal.png",
-    },
-    {
-      title: "Hadits Seeker",
-      description:
-        "An AI-based hadith search and exploration app designed to help Muslims find spiritual guidance in a more personalized way.",
-      technologies: [
-        "Next JS",
-        "AI Integration",
-        "Tailwind CSS",
-        "Responsive Design",
-      ],
-      liveUrl: "https://hadits-seeker.vercel.app/",
-      codeUrl: "https://github.com/arisandisatria/hadits-seeker.git",
-      image: "/hadits-seeker.png",
-    },
-    {
-      title: "Obatku",
-      description:
-        "Comprehensive medication management system designed to improve patient adherence through automated WhatsApp reminders, real-time progress tracking, and official SatuSehat KFA integration for seamless monitoring by healthcare professionals.",
-      technologies: ["Laravel", "Bootstrap"],
-      liveUrl: "#",
-      codeUrl: "https://github.com/arisandisatria/laravel-project.git",
-      image: "/obatku.png",
-    },
-    {
-      title: "AI Coach",
-      description:
-        "A sophisticated AI-powered android-based mobile application designed to instantly curate a curriculum of materials based on any topic desired by the user.",
-      technologies: ["React Native", "Expo", "AI Integration", "Firebase"],
-      liveUrl:
-        "https://drive.google.com/drive/folders/1OoNPjwOegMQQnmDnC4NsNev2KczUk4sy?usp=drive_link",
-      codeUrl: "https://github.com/arisandisatria/ai-coach.git",
-      image: "/ai-coach.jpg",
-    },
-  ];
+  const projects = PROJECTS;
 
   return (
     <section
